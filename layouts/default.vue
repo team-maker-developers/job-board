dcd21b9570e1b68914e8c4c88bb22f80ddd730cb
Ryo Hayase <ryo@bravo-global.co>
2020-03-05 22:06:12 +0900

    feature/7-show-job 求人情報の表示のつなぎこみ

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
       <v-toolbar-title>
         <!-- TODO: pagesで取得したデータをlayoutで表示する。実現方法が複数案あり迷うのでいったん保留 -->
         <!-- storeで実現するか？Vue Routerでマッチしたコンポーネントを取得する？ -->
-        <p class="pa-0 ma-0">港区で働く訪問看護師</p>
+        <p class="pa-0 ma-0">{{ title }}</p>
         <!-- TODO: 施設名が求人票のスキームに存在しない。問題ないか？ -->
-        <p class="pa-0 ma-0 caption">（施設名）</p>
+        <p class="pa-0 ma-0 caption">{{ name }}</p>
       </v-toolbar-title>
     </v-app-bar>
     <v-content>
@@ -20,7 +20,23 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      title: '',
+      name: ''
+    }
+  },
+  created() {
+    this.setListener()
+  },
+  methods: {
+    setListener() {
+      // emitで発火させたイベント名にする
+      this.$nuxt.$on('updateHeader', this.setHeader)
+    },
+    setHeader(title, name) {
+      this.title = title || ''
+      this.name = name || ''
+    }
   }
 }
 </script>
--- pages/jobs/_id.vue
+++ pages/jobs/_id.vue
@@ -7,17 +7,15 @@
             <v-card-text class="pb-1">
               <div class="d-flex flex-row">
                 <v-chip color="primary mx-1">
-                  <!-- TODO: IDに対応する文字列を取得して表示する -->
-                  （就業形態）
+                  正社員
                 </v-chip>
               </div>
             </v-card-text>
             <v-card-title class="mb-0 font-weight-black">
-              {{ name }}
+              {{ title }}
             </v-card-title>
             <v-card-text class="caption">
-              <!-- TODO: 施設名が求人票のスキームに存在しない。問題ないか？ -->
-              （施設名）
+              {{ name }}
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
               求人コード
@@ -31,16 +29,14 @@
               業界、業種、職種
             </v-card-title>
             <v-card-text>
-              <p class="my-2">
-                {{ industry }}
-              </p>
+              <p class="my-2">{{ industry }}、{{ occupation }}</p>
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
               仕事内容
             </v-card-title>
             <v-card-text>
               <p class="my-2">
-                {{ content }}
+                {{ job_content }}
               </p>
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
@@ -55,28 +51,19 @@
               勤務時間
             </v-card-title>
             <v-card-text>
-              <p class="my-2">
-                <!-- TODO: jsonデータを整形して表示する -->
-                {{ work_datetime }}
-              </p>
+              <p class="my-2">{{ work_start_at }}〜{{ work_end_at }}</p>
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
               給与
             </v-card-title>
             <v-card-text>
-              <p class="my-2">
-                <!-- TODO: jsonデータを整形して表示する -->
-                {{ salary }}
-              </p>
+              <p class="my-2">時給{{ salary }}円</p>
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
               契約期間
             </v-card-title>
             <v-card-text>
-              <p class="my-2">
-                <!-- TODO: jsonデータを整形して表示する -->
-                {{ contract_period }}
-              </p>
+              <p class="my-2">{{ contract_period }}まで</p>
             </v-card-text>
             <v-card-title class="blue-grey lighten-5">
               福利厚生
@@ -93,7 +80,7 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-footer fixed="true" padless="true">
+    <v-footer :fixed="true" :padless="true">
       <v-card flat tile width="100%" class="blue-grey lighten-5">
         <v-card-text>
           <div class="d-flex justify-center pb-5">
@@ -109,9 +96,63 @@
 
 <script>
 export default {
+  layout: 'default',
   async asyncData({ $axios, env, params }) {
     const data = await $axios.$get(`${env.apiDomain}/api/jobs/${params.id}`)
-    return { ...data }
+    const workStartAt =
+      `${new Date(
+        data.content.pageable_content.record.work_datetime.start_at.date
+      ).getHours()}:${new Date(
+        data.content.pageable_content.record.work_datetime.start_at.date
+      ).getMinutes()}` || ''
+    const workEndAt =
+      `${new Date(
+        data.content.pageable_content.record.work_datetime.end_at.date
+      ).getHours()}:${new Date(
+        data.content.pageable_content.record.work_datetime.end_at.date
+      ).getMinutes()}` || ''
+    const finishedAt =
+      `${new Date(
+        data.content.pageable_content.record.contract_period.finished_at.date
+      ).getMonth() + 1}月${new Date(
+        data.content.pageable_content.record.contract_period.finished_at.date
+      ).getDay()}日` || ''
+
+    const title = data.meta.title || ''
+    const name = data.company.name || ''
+    const code = data.content.pageable_content.record.code || ''
+    const jobContent = data.content.pageable_content.record.job_content || ''
+    const industry =
+      data.content.pageable_content.record.industry.industry || ''
+    const occupation =
+      data.content.pageable_content.record.industry.occupation || ''
+    const content = data.content.common_content || ''
+    const salary = data.content.pageable_content.record.salary.hourly || ''
+    const welfare = data.content.pageable_content.record.welfare || ''
+    const place = data.content.pageable_content.record.place
+    return {
+      title,
+      name,
+      code,
+      job_content: jobContent,
+      industry,
+      occupation,
+      content,
+      work_start_at: workStartAt,
+      work_end_at: workEndAt,
+      salary,
+      contract_period: finishedAt,
+      welfare,
+      place
+    }
+  },
+  mounted() {
+    this.updateHeader()
+  },
+  methods: {
+    updateHeader() {
+      this.$nuxt.$emit('updateHeader', this.title, this.name)
+    }
   }
 }
 </script>
