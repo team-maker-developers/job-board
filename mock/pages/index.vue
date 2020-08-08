fd998fbf040ac968a5a51e80cdabefa1ddbd1d03
hyomame <hyoma.0112@gmail.com>
2020-08-08 21:52:48 +0900

    mockを [こちらのページ](https://www.figma.com/file/dKMFhkyXzMN8es9rI12hFK/TeamMaker-(%E8%B6%B3%E7%AB%8B%E9%82%A6%E6%A0%84%E4%BC%9A%EF%BC%89?node-id=0%3A1) のデザインに変更しました

---
+++ mock/pages/index.vue
@@ -0,0 +1,211 @@
+<template>
+  <v-container>
+    <v-card outlined tile>
+      <v-row justify="center">
+        <v-card-title class="headline font-weight-bold">
+          社会福祉法人 足立邦栄会
+        </v-card-title>
+      </v-row>
+      <v-divider class="primary"></v-divider>
+      <div class="mx-5">
+        <v-row class="my-10">
+          <v-img src="img.png"></v-img>
+        </v-row>
+        <v-row justify="center">
+          <v-card-title class="headline font-weight-bold">
+            「もっと直接的に人の役に立てることがしたくてー<br />
+            野球一筋から介護のプロへと歩んだ20年」
+          </v-card-title>
+        </v-row>
+        <v-row justify="end">
+          <v-card-subtitle align="right" class="caption pb-0">
+            2020/07/12<br /><br />
+            介護付き有料老人ホームリブインさくら
+          </v-card-subtitle>
+          <v-card-text align="right" class="primary--text">
+            金子さん
+          </v-card-text>
+        </v-row>
+        <v-row justify="center">
+          <v-card-title class="title font-weight-bold interview-title">
+            プロ野球選手への挑戦から、介護の世界への挑戦へ！
+          </v-card-title>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            <p class="interview-question">
+              金子さんはいつから足立邦栄会にいらっしゃいますか？
+            </p>
+            <p class="interview-answer">
+              介護保険が制定された2000年から足立邦栄会にいます。一旦別の施設にも行ったのですが、リブインが立ち上がる時に戻ってきました。<br />
+              介護の仕事はもう20年になりますね。
+            </p>
+            <p class="interview-question">
+              もともと介護の道に進んだ理由はなんですか？
+            </p>
+            <p class="interview-answer">
+              実はずっと野球をやっていて。<br />
+              プロを目指して頑張っていたのですが、もっと直接的に人の役に立てることがしないなと思って。<br /><br />
+              人に直接関わる仕事に興味があったこともあり、これからの社会に必要な仕事として言われてもいたので、自らの意思で介護の道に進みました。
+            </p>
+          </v-card-text>
+        </v-row>
+        <v-row class="my-5">
+          <v-img src="img.png"></v-img>
+        </v-row>
+        <v-row>
+          <v-card-title class="title font-weight-bold interview-title">
+            20年やってきて分かった介護の専門性と本当のやりがいとは？
+          </v-card-title>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            <p class="interview-question">
+              介護の仕事に従事して20年がたちますが、仕事をする上で大切にしていることはなんですか？
+            </p>
+            <p class="interview-answer">
+              まず、たくさんのありがとうをもらうことが私たちの仕事ですよね。<br />
+              介護職の専門性は、目に見えない心情や悩みを私たちが察知して支援につなげること。<br />
+              これは本当に専門性が高いと思っています。毎日入居者と向き合っていると、「あれ、ちょっとおかしいな」とか「なにか違うな」って気づく瞬間があるんですよね。それをまず大事にすることですね。<br /><br />
+              数をこなしていくことも大事。日々の業務に追われてしまうこともあるのですが、その中での会話を大事にしていくことを心掛けていますね。
+            </p>
+            <p class="interview-question">
+              金子さんにとって足立邦栄会とはどんなところですか？
+            </p>
+            <p class="interview-answer">
+              優しいスタッフが周りにいてくれるという安心感があります。どのスタッフに話しかけても優しく返事をしてくれます。<br />
+              それが心の支えになって仕事の意欲にも繋がっているところが足立邦栄会の魅力かなと思いますね。
+            </p>
+          </v-card-text>
+        </v-row>
+        <v-row justify="center">
+          <v-card-text align="center">
+            <v-divider width="30"></v-divider>
+          </v-card-text>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            <p>
+              社会福祉法人
+              足立邦栄会では金子さんのような輝くメンバーを大募集しています！
+            </p>
+            <p>
+              私たちと一緒に足立に、いや東京に、いや日本に素敵な介護を届けていきませんか！！？
+            </p>
+            <p>
+              まずは気軽に見学やメッセージなどでお話ししましょう。ご応募お待ちしております！
+            </p>
+          </v-card-text>
+        </v-row>
+        <v-row justify="center">
+          <v-card-title class="font-weight-bold primary--text">
+            ー 募集要項 ー
+          </v-card-title>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            <p class="font-weight-bold primary--text">概要</p>
+            <p>
+              社会福祉法人足立邦栄会は、足立区、府中市、狛江市の3つの拠点、4施設で高齢者、障害者の生活支援を行っています。特別養護老人ホームやデイサービス、有料老人ホーム、地域包括支援センターなど高齢者向け施設のほか、障害者支援施設など障害者用の施設も運営しています。<br />
+              特に特別養護老人ホームさくらは、平成12年4月より介護保険法に基づき開所された施設で、東京都では初めての試みである小規模の障害者支援施設を併設しています。<br />
+              基本方針に「Heart to
+              Heart」を掲げ、ご利用されている方やご家族はもちろんのこと、職員も笑顔で生活できる施設を目指しています。
+            </p>
+            <p class="font-weight-bold primary--text">応募資格</p>
+            <p>
+              【必須 (MUST)】<br />学歴不問 / 未経験OK<br />以下の資格を有する方<br />
+              ・介護職員初任者研修修了<br />・ヘルパー2級<br />・実務研修修了<br />・介護福祉士<br />
+              入居施設介護を3年以上経験している方<br />
+              病院看護助手を経験された方大歓迎です！
+            </p>
+            <p class="font-weight-bold primary--text">想定年収 (給与詳細)</p>
+            <p>390万〜420万円<br />前職の給与を考慮します。</p>
+            <p class="font-weight-bold primary--text">選考プロセス</p>
+            <p>
+              本募集については、まずカジュアルにメッセージやお電話でお話ができればと思っています。<br />
+              その後、希望者については下記のような弊社選考の流れで進行いただくことになります。
+            </p>
+            <p>
+              １.書類選考＋面接1回<br />2.内定<br />※適性検査などはございません。
+            </p>
+            <p>#オンライン面接実施中<br />#住宅手当あり</p>
+          </v-card-text>
+        </v-row>
+        <v-row justify="center">
+          <v-card-title class="font-weight-bold primary--text">
+            ー 応募フォーム ー
+          </v-card-title>
+        </v-row>
+        <v-form>
+          <p class="mb-2">
+            お名前 <span><v-chip small color="primary">必須</v-chip></span>
+          </p>
+          <v-text-field dense outlined placeholder="田中太郎"></v-text-field>
+          <p class="mb-2">
+            年齢 <span><v-chip small color="primary">必須</v-chip></span>
+          </p>
+          <v-text-field dense outlined placeholder="半角英数"></v-text-field>
+          <p class="mb-2">
+            メールアドレス
+            <span><v-chip small color="primary">必須</v-chip></span>
+          </p>
+          <v-text-field
+            dense
+            outlined
+            placeholder="入力してください"
+          ></v-text-field>
+          <p class="mb-2">
+            メッセージ <span><v-chip small color="primary">必須</v-chip></span>
+          </p>
+          <v-textarea outlined placeholder="入力してください"></v-textarea>
+        </v-form>
+        <v-row justify="center">
+          <v-btn x-large min-width="200px" class="primary font-weight-bold">
+            送信
+          </v-btn>
+        </v-row>
+      </div>
+      <v-footer dark class="my-12">
+        <v-row justify="center" class="mt-8">
+          <p class="title font-weight-bold">
+            社会福祉法人&emsp;足立邦栄会
+          </p>
+        </v-row>
+        <v-row class="px-5 pb-5">
+          <p>〒123-0862<br />東京都足立区皿沼2-8-8</p>
+          <p>
+            電話番号：03-5691-7150<br />
+            メールアドレス：recruit@sakura.houeikai.gr.jp
+          </p>
+        </v-row>
+      </v-footer>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.interview-title {
+  padding: 0px 5px;
+  margin: 15px;
+  border-left: solid 3px #e91e63;
+  border-right: solid 3px #e91e63;
+}
+
+.interview-question {
+  padding: 15px;
+  margin: 10px 30px 10px 0px;
+  border: solid 1px #555555;
+  border-radius: 0px 20px 20px 20px;
+}
+
+.interview-answer {
+  padding: 15px;
+  margin: 10px 0px 10px 30px;
+  border: solid 1px #e91e63;
+  border-radius: 20px 0px 20px 20px;
+}
+</style>
