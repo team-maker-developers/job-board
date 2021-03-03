b30c7f5edd32a75584f385fbb6e37ac70140f1d3
nuno929 <t-mark5@kce.biglobe.ne.jp>
2021-03-03 11:33:15 +0900

    feat : wysygの描画処理の実装

---
+++ components/page-content/common-content.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-img v-if="headerImageUrl" :src="headerImageUrl" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <div v-html="contentBody" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    headerImageUrl() {
+      return this.commonContent.header_image_url
+    },
+    contentBody() {
+      return this.commonContent.body
+    },
+    ...mapState('record', ['commonContent'])
+  }
+}
+</script>
